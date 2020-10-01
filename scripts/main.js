1ed75cf28cc4731a98a1afe9e118226b9a3076ab
Arturo <frisix@gmail.com>
2020-10-01 17:07:12 +0200

    Capitulo 7

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,9 @@
 var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
-var THUMBNAL_LINK_SELECTOR = '[data-image-role="trigger"]';
+var DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]';
+var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+var TINY_EFFECT_CLASS = 'is-tiny';
+var HIDDEN_DETAIL_CLASS = 'hidden-detail';
 
 function setDetails( imageUrl , titleText ){
     'use strict';
@@ -34,16 +37,45 @@ function addThumbClickHandler(thumb){
 
         event.preventDefault();
         setDetailsFromThumb(thumb);
-        
+        showDetails();
     });
 }
 
 function getThumbnailsArray(){
 
     'use strict';
-    var thumbnails = document.querySelectorAll(THUMBNAL_LINK_SELECTOR);
+    var thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
     var thumbnailArray = [].slice.call(thumbnails);
     return thumbnailArray;
+}
+
+function hideDetails(){
+    'use strict';
+    document.body.classList.add(HIDDEN_DETAIL_CLASS);
+}
+
+function showDetails(){
+    
+    'use strict';
+    var frame = document.querySelector(DETAIL_FRAME_SELECTOR);
+    document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+    frame.classList.add(TINY_EFFECT_CLASS);
+
+    setTimeout(function(){
+        frame.classList.remove(TINY_EFFECT_CLASS);
+    },50);
+}
+
+function addKeyPressHandler(){
+    'use strict';
+    document.body.addEventListener('keyup', function(event){
+
+        event.preventDefault();
+        if (event.key === "Escape")
+        {
+            hideDetails();
+        }
+    });
 }
 
 function initializeEvents()
@@ -52,6 +84,7 @@ function initializeEvents()
     'use strict';
     var thumbnails = getThumbnailsArray();
     thumbnails.forEach(addThumbClickHandler);
+    addKeyPressHandler();
 
 }
 
