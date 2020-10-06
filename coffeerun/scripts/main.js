cd07f30dcf5f36db4f54f53a07d2a72ef9b4cec9
Arturo Candela <frisix@gmail.com>
2020-10-06 12:19:48 +0200

    Finished work of chapter 12

--- coffeerun/scripts/main.js
+++ coffeerun/scripts/main.js
@@ -6,6 +6,7 @@
     var Truck = App.Truck;
     var DataStore = App.DataStore;
     var FormHandler = App.FormHandler;
+    var Validation = App.Validation;
     var CheckList = App.CheckList;
     var myTruck = new Truck('nc-1701', new DataStore());
     window.myTruck = myTruck;
@@ -18,5 +19,8 @@
         checkList.addRow.call(checkList,data);
         
     });
+
+    formHandler.addInputHandler(Validation.isCompanyEmail);
+
     console.log(formHandler);
 })(window);
